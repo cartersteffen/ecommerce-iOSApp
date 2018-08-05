19267340bc283b2e8fa083167512c3381d5c3331
Carter Steffen <csteffe1@lamu02wq21ghtd6.local>
2018-08-04 23:49:59 -0500

    Initial Commit

---
+++ MillCityMade/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MillCityMade
+//
+//  Created by Steffen, Carter J on 8/4/18.
+//  Copyright © 2018 Steffen, Carter J. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MillCityMade/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MillCityMade
+//
+//  Created by Steffen, Carter J on 8/4/18.
+//  Copyright © 2018 Steffen, Carter J. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
