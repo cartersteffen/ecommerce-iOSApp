ce87dc0b411b37927244580168ad819df8a30daf
Carter Steffen <cartste3@gmail.com>
2018-08-06 12:27:36 -0500

    Made Contact View Responsive to different screen sizes

--- MillCityMade/FirstViewController.swift
+++ MillCityMade/FirstViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var navBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image : UIImage = UIImage(named: "Mill_City_Made_Final_Logo.png")!
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = image
+        self.navBar.topItem?.titleView = imageView
+        
+        //self.navBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 880.0)
+        //self.navigationController.navigationBar.frame = CGRectMake(0, 0, self.view.frame.size.width, 80.0)
+
     }
 
     override func didReceiveMemoryWarning() {
--- MillCityMade/SecondViewController.swift
+++ MillCityMade/SecondViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MessageUI
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailBody: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,35 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onClickButton(_ sender: UIButton) {
+        sendEmail(body: emailBody.text!)
+    }
+    
+    func sendEmail(body:String) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self as? MFMailComposeViewControllerDelegate
+            mail.setToRecipients(["cartste3@gmail.com"])
+            //mail.setMessageBody(?body<#String#>)
+            mail.setMessageBody("\(body)", isHTML: false)
+            //mail.setSubject(<#T##subject: String##String#>)
+            
+            present(mail, animated: true)
+        } else {
+            let alertController = UIAlertController(title: "Email sent failed", message:
+                "Hello, world!", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+            // show failure alert
+        }
+        emailBody.text = nil
+
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
 
 
