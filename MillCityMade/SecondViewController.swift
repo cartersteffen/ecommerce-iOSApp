774756eb0908af2aa9341d1a950044d251bf8cd9
Carter Steffen <cartste3@gmail.com>
2018-08-08 15:20:10 -0500

    Added Collection View Controller and Navigation Bar Controllers

--- MillCityMade/SecondViewController.swift
+++ MillCityMade/SecondViewController.swift
@@ -16,6 +16,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
